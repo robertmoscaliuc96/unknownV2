ff97b66c3dff7b4ce9a49ac0d58b7932815e8630
moscalir <moscalir@uni.coventry.ac.uk>
2020-09-04 21:03:19 +0300

    prepare to deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="landing-image">
+        <div className="wrapper">
+
+        <div className="navbar">
+
+          <div className="title">
+            <h2>unknown01_events</h2>
+          </div>
+
+          <div className="media">
+            <a className="icon" href="https://www.instagram.com/unknown01_events/?hl=en"><i className="fab fa-instagram"></i></a>
+
+            <a className="icon" href="https://www.facebook.com/Unknown01-101083344741492"><i className="fab fa-facebook"></i></a>
+          </div>
+
+        </div>
+
+          <div className="details">
+            <p> _ unknown forest session v2</p>
+            <p className="date"> _ 05.09</p>
+            <p> _ dm for more information</p>
+          </div>
+
+          <div className="line-up">
+              <h1>Line-up</h1>
+              <h4>./ local hosts</h4>
+              <h4>./ Aleen</h4>
+          </div>
+
+          
+        </div>
+        <h1 className="seeyou">see you soon</h1>
+        <p className="credit"><i className="far fa-copyright"></i>Designed and created by moscal</p>
+        </div>
+
     </div>
   );
 }
